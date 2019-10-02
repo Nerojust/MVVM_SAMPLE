6dc7700e162232e1a8bd083c556aee492aeaa395
nerojust <nerojust4@gmail.com>
2019-10-02 15:10:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvvmsample/ui/auth/AuthListener.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmsample.ui.auth
+
+import androidx.lifecycle.LiveData
+
+interface AuthListener {
+
+    fun onStarted()
+
+    fun onSuccess(loginResponse: LiveData<String>)
+
+    fun onFailure(message : String)
+}
---
+++ app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mvvmsample.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.mvvmsample.R
+import com.example.mvvmsample.databinding.ActivityLoginBinding
+import com.example.mvvmsample.util.hide
+import com.example.mvvmsample.util.show
+import com.example.mvvmsample.util.toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity(), AuthListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityLoginBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        progress_bar.show()
+    }
+
+    override fun onSuccess(loginResponse: LiveData<String>) {
+
+        loginResponse.observe(this, Observer {
+            progress_bar.hide()
+            toast(it)
+        })
+
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
+        progress_bar.hide()
+    }
+}
